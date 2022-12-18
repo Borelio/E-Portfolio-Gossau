19d536d9b71dba6bd86d0f1854b096a6deafe586
Nico Nussmüller <nico@nussmueller.dev>
2022-12-18 15:47:00 +0100

    feat: pictures page

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularSvgIconModule } from 'angular-svg-icon';
@@ -33,5 +33,6 @@ import { RaceComponent } from './shared/components/race/race.component';
   ],
   providers: [],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
