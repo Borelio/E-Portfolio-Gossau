5e3dced093674baa2274c7989d835f0d9ffc68b3
Nico Nussmüller <nico@nussmueller.dev>
2022-12-18 18:11:10 +0100

    feat: some home stuff

--- Frontend/src/app/pages/home/home.component.ts
+++ Frontend/src/app/pages/home/home.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RaceService } from './../../shared/services/race.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  raceService: RaceService;
+  gameErrorDelay: number = 2000;
+  gameErrorDelayOver: boolean = false;
 
+  constructor(raceService: RaceService) {
+    this.raceService = raceService;
+  }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.gameErrorDelayOver = true;
+    }, this.gameErrorDelay);
+  }
 }
