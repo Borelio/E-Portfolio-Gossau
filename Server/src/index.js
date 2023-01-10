e8d7e845a19a357fae42eab50e89c08b9f848b6d
Nico Nussmüller <nico@nussmueller.dev>
2023-01-10 21:24:46 +0100

    feat: better override

--- Server/src/index.js
+++ Server/src/index.js
@@ -80,59 +80,52 @@ app.post('/redirect/all', (req, res) => {
     res.send('Okay');
 });
 
-const firstOverrideOption = {
-    "mainImage": "https://www.lto.de/fileadmin/_processed_/8/c/csm_mittelfinger_535_d949ed423d.jpg",
-    "image1": "assets/images/picture1.jpg",
-    "image1Edited": "https://t3.ftcdn.net/jpg/00/26/10/38/360_F_26103851_7ncChqQJfxvwjsvTUXJYI2EpaVpX7M84.jpg",
-    "image2": "assets/images/picture2.jpg",
-    "image2Edited": "https://www.imago-images.de/bild/st/0116849452/s.jpg",
-    "image3": "assets/images/picture3.jpg",
-    "image3Edited": "https://st.depositphotos.com/1049680/2347/i/600/depositphotos_23474076-stock-photo-human-hand-gesturing-with-middle.jpg",
-    "whyText": "Ich hasse es",
-    "gameText": "Ich mag jedoch mein Game xD",
-    "kaboomSound": "https://gossau-extension.nussmueller.dev/im-karton.mp3",
-    "honkSound": "https://gossau-extension.nussmueller.dev/aaaa.mp3",
-    "videoIframeCode": '<iframe width="1280" height="720" src="https://www.youtube.com/embed/eEa3vDXatXg" title="Why are u gae????" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'
-};
-const seccondOverrideOption = {
-    "mainImage": "https://www.plantura.garden/wp-content/uploads/2017/03/baobabbaum.jpg",
-    "image1": "https://www.baumpflegeportal.de/wp-content/uploads/2016/05/160509_Starke-Baumtypen_Eiche-im-Seidengewand_03.jpg",
-    "image1Edited": "https://www.baumpflegeportal.de/wp-content/uploads/2016/05/160509_Starke-Baumtypen_Eiche-im-Seidengewand_02.jpg",
-    "image2": "https://www.lamodula.at/media/magefan_blog/esche-holz-baum-1_2_.jpeg",
-    "image2Edited": "https://www.plantura.garden/wp-content/uploads/2017/03/baobabbaum.jpg",
-    "image3": "https://images.squarespace-cdn.com/content/v1/60472827fe54df702f7eb4c7/1617090977550-OBXG6PPUEXFZE47TBMQB/was-kostet-ein-baum-sommerlinde-2-baumschule-oekoplant-wels.jpg",
-    "image3Edited": "https://www.waldwissen.net/assets/waldwirtschaft/schaden/trockenheit/wsl_baumartenwahl_trockenheit/wsl_baumartenwahl_trockenheit_waldfoehre.jpeg",
-    "whyText": "Bäum sind halt scho cool und de anderi Nico het das jo scho guet zeiget mit sinere Siite xD",
-    "gameText": "Leider han ich keis Game mit Bäum aber defür mit Autos",
-    "kaboomSound": "https://gossau-extension.nussmueller.dev/Kettensäge.mp3",
-    "honkSound": "https://gossau-extension.nussmueller.dev/BaumKaputt.mp3",
-    "videoIframeCode": '<iframe width="1280" height="960" src="https://www.youtube.com/embed/2GMU58e60x4" title="Hermann Schönbächler best of" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'
-};
-const thirdOverrideOption = {
-    "mainImage": "assets/images/picture1_small.jpg",
-    "image1": "assets/images/picture1.jpg",
-    "image1Edited": "assets/images/picture1_edited.jpg",
-    "image2": "assets/images/picture2.jpg",
-    "image2Edited": "assets/images/picture2_edited.jpg",
-    "image3": "assets/images/picture3.jpg",
-    "image3Edited": "assets/images/picture3_edited.jpg"
-};
+const options = [
+    {
+        "mainImage": "https://www.lto.de/fileadmin/_processed_/8/c/csm_mittelfinger_535_d949ed423d.jpg",
+        "image1": "assets/images/picture1.jpg",
+        "image1Edited": "https://t3.ftcdn.net/jpg/00/26/10/38/360_F_26103851_7ncChqQJfxvwjsvTUXJYI2EpaVpX7M84.jpg",
+        "image2": "assets/images/picture2.jpg",
+        "image2Edited": "https://www.imago-images.de/bild/st/0116849452/s.jpg",
+        "image3": "assets/images/picture3.jpg",
+        "image3Edited": "https://st.depositphotos.com/1049680/2347/i/600/depositphotos_23474076-stock-photo-human-hand-gesturing-with-middle.jpg",
+        "whyText": "Ich hasse es",
+        "gameText": "Ich mag jedoch mein Game xD",
+        "kaboomSound": "https://gossau-extension.nussmueller.dev/im-karton.mp3",
+        "honkSound": "https://gossau-extension.nussmueller.dev/aaaa.mp3",
+        "videoIframeCode": '<iframe width="1280" height="720" src="https://www.youtube.com/embed/eEa3vDXatXg" title="Why are u gae????" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'
+    },
+    {
+        "mainImage": "https://www.plantura.garden/wp-content/uploads/2017/03/baobabbaum.jpg",
+        "image1": "https://www.baumpflegeportal.de/wp-content/uploads/2016/05/160509_Starke-Baumtypen_Eiche-im-Seidengewand_03.jpg",
+        "image1Edited": "https://www.baumpflegeportal.de/wp-content/uploads/2016/05/160509_Starke-Baumtypen_Eiche-im-Seidengewand_02.jpg",
+        "image2": "https://www.lamodula.at/media/magefan_blog/esche-holz-baum-1_2_.jpeg",
+        "image2Edited": "https://www.plantura.garden/wp-content/uploads/2017/03/baobabbaum.jpg",
+        "image3": "https://images.squarespace-cdn.com/content/v1/60472827fe54df702f7eb4c7/1617090977550-OBXG6PPUEXFZE47TBMQB/was-kostet-ein-baum-sommerlinde-2-baumschule-oekoplant-wels.jpg",
+        "image3Edited": "https://www.waldwissen.net/assets/waldwirtschaft/schaden/trockenheit/wsl_baumartenwahl_trockenheit/wsl_baumartenwahl_trockenheit_waldfoehre.jpeg",
+        "whyText": "Bäum sind halt scho cool und de anderi Nico het das jo scho guet zeiget mit sinere Siite xD",
+        "gameText": "Leider han ich keis Game mit Bäum aber defür mit Autos",
+        "kaboomSound": "https://gossau-extension.nussmueller.dev/Kettensäge.mp3",
+        "honkSound": "https://gossau-extension.nussmueller.dev/BaumKaputt.mp3",
+        "videoIframeCode": '<iframe width="1280" height="960" src="https://www.youtube.com/embed/2GMU58e60x4" title="Hermann Schönbächler best of" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'
+    },
+    {
+        "forceRedirect": "https://gossau.nussmueller.dev/"
+    },
+    {
+        "forceRedirect": "http://www.austrasse20.ch/Familienfasnacht/"
+    },
+    {
+        "forceRedirect": "http://www.austrasse20.ch/cgi-bin/gb.php?id=3124"
+    }
+];
 
 let setOverrideAction = 0;
 app.get('/overrideUrls', (req, res) => {
-    switch (setOverrideAction) {
-        case 1:
-            res.json(firstOverrideOption);
-            break;
-        case 2:
-            res.json(seccondOverrideOption);
-            break;
-        case 3:
-            res.json(thirdOverrideOption);
-            break;
-        default:
-            res.json({});
-            break;
+    if (setOverrideAction === 0) {
+        res.json({});
+    } else {
+        res.json(options[setOverrideAction]);
     }
 
     //"extraScriptCode": "https://gossau-extension.nussmueller.dev/stupidscript.js"
@@ -144,7 +137,7 @@ app.post('/setoverrideaction', (req, res) => {
         return;
     }
 
-    if (req.query.hasOwnProperty('nr') && +req.query.nr > 0) {
+    if (req.query.hasOwnProperty('nr') && +req.query.nr > 0 && +req.query.nr < options.length) {
         setOverrideAction = +req.query.nr;
         io.emit('reloadUrls');
         res.send('Okay');
